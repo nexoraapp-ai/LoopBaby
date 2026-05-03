30db1cbbb8f91c3fa18d8be3bda6cc713fff346d
loop-baby-test <xxmanuelvalente@gmail.com>
2026-05-03 14:25:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -129,6 +129,7 @@ if st.session_state.page == "Login":
                     st.error("Telefono errato")
             else:
                 st.error("Utente non trovato")
+    st.stop()
 
     # ===== REGISTRAZIONE =====
     with tab2:
